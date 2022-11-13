7e6abaad3c767c737a1f217070204428286b6094
Renato Silva <renato_yumi@hotmail.com>
2022-11-13 05:40:56 -0300

    wip

---
+++ test/Models/Usuario.cs
@@ -0,0 +1,15 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+
+namespace test_with_mongo.Models
+{
+    public class Usuario
+    {
+        [BsonId]
+        [BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)]
+        public string? Id { get; set; }
+
+        public string? Name { get; set; } = null;
+
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+    }
+}
---
+++ test/Program.cs
@@ -0,0 +1,33 @@
+﻿using MongoDB.Driver;
+using test_with_mongo.Models;
+
+MongoUrl connectionString = new MongoUrl("mongodb+srv://erp-system:<YOUR_PASSWORD_GOES_HERE>@cluster0.fiotm.mongodb.net/?retryWrites=true&w=majority");
+string databaseName = "YOUR_DATABASE_NAME_SHOULD_BE_CREATED_BEFORE";
+string collectionName = "users";
+
+MongoClient client = new MongoClient(connectionString);
+
+IMongoDatabase db = client.GetDatabase(databaseName);
+IMongoCollection<Usuario> collection = db.GetCollection<Usuario>(collectionName);
+
+List<Usuario> users = new List<Usuario>() {
+new Usuario(){ Name="Bill"},
+new Usuario(){ Name="James"},
+new Usuario(){ Name="Jonas"},
+new Usuario(){ Name="Ryan"},
+new Usuario(){ Name="Robb"},
+new Usuario(){ Name="Joe"},
+new Usuario(){ Name="Dennis"},
+new Usuario(){ Name="Markus"},
+new Usuario(){ Name="Iain"},
+new Usuario(){ Name="Logan"},
+};
+
+await collection.InsertManyAsync(users);
+
+var results = await collection.FindAsync(_ => true);
+
+foreach (Usuario usuario in results.ToList())
+{
+    Console.WriteLine(value: $"{usuario.Id}: {usuario.Name}");
+}
